ff26f670758e18e686059512929657a374b184c2
20chan <2@0chan.dev>
2020-08-27 07:27:01 +0000

    implement basic server

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import express, { response } from "express";
+import * as bodyParser from "body-parser";
+import arrow from "./routes/arrow";
+
+const app = express();
+const PORT = process.env.PORT || 12000;
+
+app.use(bodyParser.json());
+
+app.get("/api/health", (req, res) => {
+    response.send("healthy");
+});
+
+app.use("/api/arrow", arrow);
+
+const server = app.listen(PORT, () => {
+    console.log(`server started at localhost:${PORT}`);
+});
+
